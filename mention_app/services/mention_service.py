0cb3a4faf553c33d9cb49e61a74c404ffdc0346d
radhikasharm123 <159181609+radhikasharm123@users.noreply.github.com>
2024-11-21 16:02:52 +0530

    Update mention_service.py

--- mention_app/services/mention_service.py
+++ mention_app/services/mention_service.py
@@ -1,13 +1,4 @@
-"""from shared.models.mention_model import Mention
-from shared.utils.db_utils import db
 
-class MentionService:
-    @staticmethod
-    def create_mention(mentioned_user_id, comment_id):
-        mention = Mention(mentioned_user_id=mentioned_user_id, comment_id=comment_id)
-        db.session.add(mention)
-        db.session.commit()
-        return mention"""
 
 from shared.models.mention_model import Mention
 from shared.utils.db_utils import db
